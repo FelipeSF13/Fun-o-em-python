04ec075c22c747020f2f2841ee8fd0a63824f50f
Felipe S. Freitas <133303018+FelipeSF13@users.noreply.github.com>
2023-10-22 13:13:04 -0300

    Add files via upload

---
+++ função.py
@@ -0,0 +1,44 @@
+# Função para adição
+def adicao(a, b):
+    return a + b
+
+# Função para subtração
+def subtracao(a, b):
+    return a - b
+
+# Função para multiplicação
+def multiplicacao(a, b):
+    return a * b
+
+# Função para divisão
+def divisao(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero"
+    return a / b
+
+# Função principal da calculadora
+def calculadora():
+    print("Selecione a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
+    escolha = input("Digite o número da operação desejada: ")
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if escolha == '1':
+        print("Resultado:", adicao(num1, num2))
+    elif escolha == '2':
+        print("Resultado:", subtracao(num1, num2))
+    elif escolha == '3':
+        print("Resultado:", multiplicacao(num1, num2))
+    elif escolha == '4':
+        print("Resultado:", divisao(num1, num2))
+    else:
+        print("Opção inválida")
+
+# Chame a função da calculadora
+calculadora()
